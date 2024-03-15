2122dba78eea81f70538c568e37433a429e5e3c7
Stephen Adesina <bauerstevenson@gmail.com>
2024-03-15 16:22:38 +0200

    making app github actions compliant

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
